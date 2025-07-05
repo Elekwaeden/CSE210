85f3e6db5cf92ba6bddcf900b92882203420a9ef
Elekwaeden <edenchijiokeokoriee@gmail.com>
2025-07-05 21:34:03 +0100

    finished prep1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // This program prompts the user for their first and last name
+        // and stores them in variables.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}"Eden);
     }
 }
