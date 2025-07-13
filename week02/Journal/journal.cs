c13f0b406df7dc38158fa63caa797700f155bc2c
Elekwaeden <edenchijiokeokoriee@gmail.com>
2025-07-13 23:27:47 +0100

    project A

---
+++ week02/Journal/journal.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+public class Journal
+{
+    private List<Entry> _entries;
+
+    public Journal()
+    {
+        _entries = new List<Entry>();
+    }
+
+    public void WriteNewEntry()
+    {
+        
+    }
+
+    public void DisplayJournal()
+    {
+        
+    }
+
+    public void SaveToFile(string filename)
+    {
+        
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        
+    }
+}
