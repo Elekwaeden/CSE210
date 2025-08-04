1eb2ed4fe9654bc7eb012e62527d9581e16093ae
Elekwaeden <edenchijiokeokoriee@gmail.com>
2025-08-04 19:20:38 +0100

    assignment done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Base Assignment test
+        Assignment assignment = new Assignment("Eden", "Chemical Reactions");
+        Console.WriteLine(assignment.GetSummary());
+
+        // MathAssignment test
+        MathAssignment mathAssignment = new MathAssignment("Eden", "Algebra", "5.3", "1-10, 15");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        // WritingAssignment test
+        WritingAssignment writingAssignment = new WritingAssignment("Eden", "Literature", "The Role of Symbolism in Poetry");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
+
